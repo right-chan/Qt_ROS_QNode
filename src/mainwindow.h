bd79c79126974de61549276864c5a1d9526884be
HeeChan <1997hc@naver.com>
2021-11-12 01:47:48 +0900

    QNode

---
+++ src/mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include "qnode.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(int argc, char** argv, QWidget *parent = 0);
+    ~MainWindow();
+    QString toQString(std::string const &s);
+    std::string fromQString(QString const &s);
+
+public slots:
+    void updateSubBox();
+
+
+private slots:
+    void on_publishButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QNode qnode;
+
+};
+
+#endif // MAINWINDOW_H
