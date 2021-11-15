ed8b6eb923aa73b55a9ea69f55e8bfcf8057ee63
HeeChan <1997hc@naver.com>
2021-11-15 14:12:09 +0900

    play video

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,12 +2,33 @@
 #include "ui_mainwindow.h"
 #include <QtWidgets>
 
+#include <QMediaPlayer>
+#include <QVideoWidget>
+
 MainWindow::MainWindow(int argc, char** argv, QWidget *parent) :
     QMainWindow(parent), qnode(argc, argv),
     ui(new Ui::MainWindow)
 {
     qnode.init();
     ui->setupUi(this);
+//  이미지
+    QPixmap pix("/home/chan/catkin_ws/src/qt_test/src/dice.png");
+    int w = ui->imglabel1->width();
+    int h = ui->imglabel1->height();
+    ui->imglabel1->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
+//  움짤 QMovie .gif
+    QMovie *movie = new QMovie("/home/chan/catkin_ws/src/qt_test/src/rotating_earth.gif");
+    ui->imglabel2->setMovie(movie);
+    movie->start();
+//  mp4
+    QMediaPlayer * player = new QMediaPlayer;
+    QVideoWidget * vw = new QVideoWidget;
+    player->setVideoOutput(vw);
+    player->setMedia(QUrl::fromLocalFile("/home/chan/catkin_ws/src/qt_test/src/strawberry moon.mp4"));
+
+    vw->setGeometry(100,100,300,400);
+    vw->show();
+    player->play();
 
     QObject::connect(&qnode, SIGNAL(msgSubscribed()), this, SLOT(updateSubBox()));
 }
