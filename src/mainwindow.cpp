ba41a539261ba04f401074f452d793a502ac2b49
HeeChan <1997hc@naver.com>
2021-11-16 04:59:44 +0900

    whatimdoing?

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,28 +10,46 @@ MainWindow::MainWindow(int argc, char** argv, QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     qnode.init();
+    this->setStyleSheet("background-color: green;");
     ui->setupUi(this);
+    
 //  이미지
-    QPixmap pix("/home/chan/catkin_ws/src/qt_test/src/dice.png");
+    QPixmap *pix;
+    pix = new QPixmap("/home/chan/catkin_ws/src/qt_test/src/dice.png");
     int w = ui->imglabel1->width();
     int h = ui->imglabel1->height();
-    ui->imglabel1->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
-//  움짤 QMovie .gif
-    QMovie *movie = new QMovie("/home/chan/catkin_ws/src/qt_test/src/rotating_earth.gif");
-    ui->imglabel2->setMovie(movie);
-    movie->start();
-//  mp4
-    QMediaPlayer * player = new QMediaPlayer(this);
-    QVideoWidget * vw = new QVideoWidget(this->ui->imglabel3);
-    // ui->imglabel3->setMovie(player);
-    player->setVideoOutput(vw);
-    player->setMedia(QUrl::fromLocalFile("/home/chan/catkin_ws/src/qt_test/src/strawberry moon.mp4"));
+    ui->imglabel1->setPixmap(pix->scaled(w,h,Qt::KeepAspectRatio));
+    ui->imglabel1->show();
 
-    vw->setGeometry(100,100,300,400);
-    vw->show();
-    player->play();
+//  움짤 QMovie .gif
+    movie  = new QMovie("/home/chan/catkin_ws/src/qt_test/src/dice.png");
+    QMovie *movie2 = new QMovie("/home/chan/catkin_ws/src/qt_test/src/loading.gif");
+    movie2->setScaledSize(this->ui->imglabel3->size());
+    ui->imglabel2->setMovie(movie2);
+    // movie2->setSpeed(200);
+    // ui->imglabel2->resize(1920,1080);
+    // ui->imglabel2->showFullScreen();
+    // label.setMovie(movie2);
+    label = new QLabel(this);
+    label->setMovie(movie2);
+    label->setGeometry(1000,0,480,300);
+    movie2->start();
+    
+
+
+
+//  mp4
+    player = new QMediaPlayer(this);
+    vw = new QVideoWidget(this);
+    player->setMedia(QUrl::fromLocalFile("/home/chan/catkin_ws/src/qt_test/src/strawberry moon.mp4"));
+    player->setVideoOutput(vw);
+    vw->setGeometry(300,300,1920,1080);
+    // vw->setFullScreen(true);
+    // player->play();
+    // player->stop();
 
     QObject::connect(&qnode, SIGNAL(msgSubscribed()), this, SLOT(updateSubBox()));
+    QObject::connect(ui->exitbutton, &QPushButton::clicked, this, &QApplication::quit);
 }
 
 MainWindow::~MainWindow()
@@ -54,6 +72,40 @@ std::string MainWindow::fromQString(QString const &s)
 void MainWindow::updateSubBox(){
     std::string subbed_msg = qnode.sub_message;
     QString outQString = toQString(subbed_msg);
+    // int msg = outQString.toInt;
+    switch(outQString.toInt())
+    {
+        case 1:
+        std::cout<<"case 1"<<std::endl;
+        player->play();
+
+        break;
+        case 2:
+        std::cout<<"case 2"<<std::endl;
+        player->stop();
+        break;
+        case 3:
+        std::cout<<"case 3"<<std::endl;
+        player->pause();
+        break;
+        case 4:
+        std::cout<<"case 4"<<std::endl;
+        label->setMovie(movie);
+        movie->start();
+        break;
+        case 5:
+        std::cout<<"case 5"<<std::endl;
+        player->pause();
+        break;
+        case 6:
+        std::cout<<"case 6"<<std::endl;
+        player->pause();
+        break;
+        default:
+        std::cout<<"deafult"<<std::endl;
+        break;
+    }
+    
     ui->subText->setPlainText(outQString);
 }
 
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,6 +4,10 @@
 #include <QMainWindow>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <QMediaPlayer>
+#include <QVideoWidget>
+#include <QtWidgets>
+
 #include "qnode.h"
 
 namespace Ui {
@@ -20,17 +24,20 @@ public:
     QString toQString(std::string const &s);
     std::string fromQString(QString const &s);
 
-public slots:
-    void updateSubBox();
-
-
-private slots:
-    void on_publishButton_clicked();
-
 private:
     Ui::MainWindow *ui;
     QNode qnode;
 
+    QVideoWidget * vw;
+    QMediaPlayer * player;   
+    QLabel * label;
+    QMovie *movie;
+
+public slots:
+    void updateSubBox();
+
+private slots:
+    void on_publishButton_clicked();
 };
 
 #endif // MAINWINDOW_H
